b98798cdd2ebf87bc1a469eaed237c12de1081a5
BuiThanhBaVuong <bavuongco10@gmail.com>
2019-11-30 14:14:29 +0700

    init

---
+++ cypress/integration/examples/actions.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+context('Example', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5000')
+  });
+
+
+  it('have title', () => {
+    cy.title().should('eq', 'meteor-cypress-circle-ci-integration');
+    cy.get('#react-target').contains('Learn Meteor!');
+  })
+});
